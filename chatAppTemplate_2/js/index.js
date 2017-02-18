c432b13a249595ce0e73557aaa0f3a84b0a9a0bd
Adam Carlton <acarlton@uoregon.edu>
2017-02-18 15:28:46 -0800

    removed a lot of extraneous BS from the HTML and JS

--- chatAppTemplate_2/js/index.js
+++ chatAppTemplate_2/js/index.js
@@ -1,5 +1,6 @@
 (function() {
-  var NYLM, claerResizeScroll, conf, getRandomInt, insertI, lol;
+  var clearResizeScroll, sendMessage, months;
+
 
   conf = {
     cursorcolor: "#696c75",
@@ -13,43 +14,35 @@
     cursorborder: "none"
   };
 
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 
-
-  getRandomInt = function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  claerResizeScroll = function() {
-    $("#texxt").val("");
+  clearResizeScroll = function() {
+    $("#text").val("");
     $(".messages").getNiceScroll(0).resize();
     return $(".messages").getNiceScroll(0).doScrollTop(999999, 999);
   };
 
-  insertI = function() {
-    var innerText, otvet;
-    innerText = $.trim($("#texxt").val());
+  sendMessage = function() {
+    var innerText;
+    innerText = $.trim($("#text").val());
     if (innerText !== "") {
-      $(".messages").append("<li class=\"i\"><div class=\"head\"><span class=\"time\">" + (new Date().getHours()) + ":" + (new Date().getMinutes()) + " AM, Today</span><span class=\"name\"> </span></div><div class=\"message\">" + innerText + "</div></li>");
-      claerResizeScroll();
-      return otvet = setInterval(function() {
-        $(".messages").append("<li class=\"friend-with-a-SVAGina\"><div class=\"head\"><span class=\"name\"> </span><span class=\"time\">" + (new Date().getHours()) + ":" + (new Date().getMinutes()) + " AM, Today</span></div><div class=\"message\">" + NYLM[getRandomInt(0, NYLM.length - 1)] + "</div></li>");
-        claerResizeScroll();
-        return clearInterval(otvet);
-      }, getRandomInt(2500, 500));
+      $(".messages").append("<li class=\"i\"><div class=\"head\"><span class=\"time\">"+months[new Date().getUTCMonth()]+" "+new Date().getDate()+" "+new Date().getHours()+ ":"+new Date().getUTCMinutes()+"</span><span class=\"name\"></span></div><div class=\"message\">"+innerText+"</div></li>");
+      clearResizeScroll();
+
     }
   };
 
   $(document).ready(function() {
     $(".list-friends").niceScroll(conf);
     $(".messages").niceScroll(lol);
-    $("#texxt").keypress(function(e) {
+    $("#text").keypress(function(e) {
       if (e.keyCode === 13) {
-        insertI();
+        sendMessage();
         return false;
       }
     });
     return $(".send").click(function() {
-      return insertI();
+      return sendMessage();
     });
   });
 
