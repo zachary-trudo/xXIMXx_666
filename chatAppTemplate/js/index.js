fc86d0eebf14bd2df8ee3a430bb7dda9e08e534c
Adam Carlton <acarlton@uoregon.edu>
2017-02-19 16:43:14 -0800

    Chat template (#7)
    * current chat template
    
    renamed folder
    
    * renamed folder
    
    current chat template
    
    removed duplicate folder
    
    squashed commits

---
+++ chatAppTemplate/js/index.js
@@ -0,0 +1,49 @@
+(function() {
+  var clearResizeScroll, sendMessage, months;
+
+
+  conf = {
+    cursorcolor: "#696c75",
+    cursorwidth: "4px",
+    cursorborder: "none"
+  };
+
+  lol = {
+    cursorcolor: "#cdd2d6",
+    cursorwidth: "4px",
+    cursorborder: "none"
+  };
+
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+  clearResizeScroll = function() {
+    $("#text").val("");
+    $(".messages").getNiceScroll(0).resize();
+    return $(".messages").getNiceScroll(0).doScrollTop(999999, 999);
+  };
+
+  sendMessage = function() {
+    var innerText;
+    innerText = $.trim($("#text").val());
+    if (innerText !== "") {
+      $(".messages").append("<li class=\"i\"><div class=\"head\"><span class=\"time\">"+months[new Date().getUTCMonth()]+" "+new Date().getDate()+" "+new Date().getHours()+ ":"+new Date().getUTCMinutes()+"</span><span class=\"name\"></span></div><div class=\"message\">"+innerText+"</div></li>");
+      clearResizeScroll();
+
+    }
+  };
+
+  $(document).ready(function() {
+    $(".list-friends").niceScroll(conf);
+    $(".messages").niceScroll(lol);
+    $("#text").keypress(function(e) {
+      if (e.keyCode === 13) {
+        sendMessage();
+        return false;
+      }
+    });
+    return $(".send").click(function() {
+      return sendMessage();
+    });
+  });
+
+}).call(this);
